5c567d1c7bba87760a507e6f96458740e7cc56f2
安忆 <i@anyi.in>
2026-01-03 19:04:42 +0800

    build: add conditional linting and TypeScript error handling

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,15 @@
 import { type NextConfig } from 'next';
+import { env } from 'node:process';
 
-const nextConfig: NextConfig = { output: 'export' };
+const skipLint = env.NODE_ENV === 'production' && Boolean(env['SKIP_LINT']);
+
+const nextConfig: NextConfig = {
+	output: 'export',
+
+	eslint: { ignoreDuringBuilds: skipLint },
+	typescript: { ignoreBuildErrors: skipLint },
+
+	experimental: { webpackMemoryOptimizations: skipLint },
+};
 
 export default nextConfig;
