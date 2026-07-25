903436bc1a911d324b73c162b861588803dde56d
Shion <102428923+ShionKanagawa@users.noreply.github.com>
2026-07-25 15:14:26 +0800

    chores: v0.10.7 -> v0.10.8

--- src/data/announcement.ts
+++ src/data/announcement.ts
@@ -1,4 +1,4 @@
-export const ANNOUNCEMENT_VERSION = 'v0.10.7-2026-06-02';
+export const ANNOUNCEMENT_VERSION = 'v0.10.8-2026-07-25';
 
 export const ANNOUNCEMENT_TITLE = 'MetaMystia ResourceEx Editor 公告';
 
@@ -13,6 +13,7 @@ export const ANNOUNCEMENT_SECTIONS: AnnouncementSection[] = [
 	{
 		title: '近期更新',
 		items: [
+			'对话编辑器中允许设计对话「选项分支」、「跳转」、和「结束」',
 			'允许在 Mission 中设计「还债(BillRepayment)」类型的完成条件',
 			'对话编辑器中的 CG/BG/音效选择 新增资产管理器浏览按钮，点击可打开弹窗以选择资产文件。',
 			'角色列表增加特殊顾客「秦心」',
